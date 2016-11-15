6ab2487b9c372328e8949ec9e95fac99d07f48d5
shine636363 <shine636363@sina.com>
2016-11-15 03:38:21 +0000

    more edition

--- code/init_tracker.m
+++ code/init_tracker.m
@@ -5,26 +5,30 @@ v{1}=VideoReader('../CLTdataset/dataset3-4cameras/Cam1.avi');
 v{2}=VideoReader('../CLTdataset/dataset3-4cameras/Cam2.avi');
 v{3}=VideoReader('../CLTdataset/dataset3-4cameras/Cam3.avi');
 v{4}=VideoReader('../CLTdataset/dataset3-4cameras/Cam4.avi');
-num = 0;
-for i=1:4
-    if  v{i}.NumberOfFrames>num
-         num = v{i}.NumberOfFrames;
-    end 
-end
 
 % load annotations
 annotation_path='../CLTdataset/annotation_files/annotation/Dataset3/at least 4/';
 annotation_files = dir([annotation_path '*.mat']);
 
 %load cameras' detection
-cam{1}=load('../CLTdataset/annotation_files/annotation/Dataset3/Cam1.dat');
-cam{2}=load('../CLTdataset/annotation_files/annotation/Dataset3/Cam2.dat');
-cam{3}=load('../CLTdataset/annotation_files/annotation/Dataset3/Cam3.dat');
-cam{4}=load('../CLTdataset/annotation_files/annotation/Dataset3/Cam4.dat');
-% for i=1:4
-%     data{i}=struct('frameindex',[],'id',[],'gt',zeros(1,4),'feature_set',[]);
-%     for j=1:size(cam{i},1)
-%        data{i}(j).frameindex=cam{i}(j,2);
-%        data{i}(j).id=cam{i}(j,3);
-%     end
-% end
+cam{1}  = load('../CLTdataset/annotation_files/annotation/Dataset3/Cam1.dat');
+cam{2}  = load('../CLTdataset/annotation_files/annotation/Dataset3/Cam2.dat');
+cam{3}  = load('../CLTdataset/annotation_files/annotation/Dataset3/Cam3.dat');
+cam{4}  = load('../CLTdataset/annotation_files/annotation/Dataset3/Cam4.dat');
+
+org_det  = [cam{1}; cam{2}; cam{3}; cam{4}]; % sort out sequential orders
+[~, idd] = sort(org_det(:, 2)); 
+det      = org_det(idd, :);
+numP     = max(org_det(:, 3)); % number of pedestrian
+
+% load fast VGG
+vl_setupnn()
+opts.modelPath = fullfile(fileparts(mfilename('fullpath')), ...
+'../..', 'models', 'fast-rcnn-vgg16-pascal07-dagnn.mat') ;
+opts.gpu = [] ;
+net = load(opts.modelPath) ;
+net = dagnn.DagNN.loadobj(net);
+net.mode = 'test' ; 
+
+% init parameters
+flag_single = 1; % 0 : multi-cameras; 1 : single-camera
--- run_CLT.m
+++ run_CLT.m
@@ -7,64 +7,77 @@ function run_CLT()
 warning off
 addpath(genpath('./'))
 init_tracker
-% load fast VGG
-run(fullfile(fileparts(mfilename('fullpath')), ...
-   'externel','matconvnet', 'matlab', 'vl_setupnn.m')) ;
-opts.modelPath = fullfile(fileparts(mfilename('fullpath')), ...
-'..', 'models', 'fast-rcnn-vgg16-pascal07-dagnn.mat') ;
-opts.gpu = [] ;
-net = load(opts.modelPath) ;
-net = dagnn.DagNN.loadobj(net);
-net.mode = 'test' ; 
-% -------------------------
-idf=0;
-id_feat=[];
-track=[];
-non_init=true;
-appear=false;
-%struct for gt and  features of detections in 4 cameras 
-for i=1:4
-    data{i}=struct('gt',cam{i},'feature_set',[]);
-end
-% load image 
-for k = 1 : num
-    x=cell([],4);
-    for i=1:4
-        frame{i}= read(v{i},k); 
-        x{i}=find(data{i}.gt(:,2)==k-1);% find whether there are detections in single frame
-        if ~isempty(x{i})
-            detections=data{i}.gt(x{i},:);
-            % extract the CNN features (using VGG model)
-            f=get_features(frame{i},detections,net);
-            data{i}.feature_set(x{i},:)=f;            
-            p=find(detections(:,3)==idf, 1);
-            if ~isempty(p)
-                if non_init  %initialize the cam and position of target
-                    res=detections(find(detections(:,3)==idf, 1),:);
-                    non_init=false;
-                    appear=true;
-                end
-                id_feat=[id_feat;f(p,:)];
-                id_feat_mean=sum(id_feat,1)/size(id_feat,1);
-            end  
-        end                          
-    end
-     % tracking
-     if non_init
-         go to disp_result
-     else
-         if appear
-            [appear, res]=single_camera_tracker(data,res,idf,x);           
-        else 
-            [appear, res]=inter_camera_tracker(data,x,id_feat_mean);
-        end
-     end    
-    track=[track;res];
-    -------------------------------------------
-    show detection
-     disp_results    
 
-end
+% train tracker at 1st frame
+for idP = 0: 1: numP
     
+    % read init pedestrian detection
+    ids      = find(det(:, 3) == idP);  
+    active_c = det(ids(1), 1);        % active camera
+    start_f  = det(ids(1), 2) + 1;    % starting frame
     
+    % train tracker: extract CNN reference feature
+    img      = read(v{active_c}, start_f); 
+    bbox     = cam{active_c}(start_f, :);
+    feat_ref = get_features(img, bbox, net);
+    
+    % tracking
+    idf = start_f + 1;
+    while idf < v{active_c}.NumberOfFrames
+        
+        % read detections
+        if flag_single
+            % read detections from one camera
+        else
+            % read detections from all cameras
+            
+        end         
+        
+        % extract CNN features
+        
+        % matching
+        
+        % find id
+        
+        
+    end
 end
+
+% % load image 
+% for k = 1 : num
+%     x = cell([],4); 
+%     for i=1:4
+%         frame{i}= read(v{i},k); 
+%         x{i}=find(data{i}.gt(:,2)==k-1);% find whether there are detections in single frame
+%         if ~isempty(x{i})
+%             detections=data{i}.gt(x{i},:);
+%             % extract the CNN features (using VGG model)
+%             f = get_features(frame{i},detections, net);
+%             data{i}.feature_set(x{i},:)=f;            
+%             p=find(detections(:,3)==idf, 1);
+%             if ~isempty(p)
+%                 if non_init  %initialize the cam and position of target
+%                     res=detections(find(detections(:,3)==idf, 1),:);
+%                     non_init=false;
+%                     appear=true;
+%                 end
+%                 id_feat=[id_feat;f(p,:)];
+%                 id_feat_mean=sum(id_feat,1)/size(id_feat,1);
+%             end  
+%         end                          
+%     end
+%      % tracking
+%      if non_init
+%          go to disp_result
+%      else
+%          if appear
+%             [appear, res]=single_camera_tracker(data,res,idf,x);           
+%         else 
+%             [appear, res]=inter_camera_tracker(data,x,id_feat_mean);
+%         end
+%      end    
+% %      track=[track;res];
+%     disp_results    
+% end  
+%     
+% end
