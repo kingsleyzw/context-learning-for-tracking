3aad6d71b12f11c20f9a0ae462239b75ab205f2a
Jingjing Xiao <shine636363@sina.com>
2016-11-30 01:19:33 +0000

    JJ comments

--- code/get_features.m
+++ code/get_features.m
@@ -1,10 +1,10 @@
-function f = get_features(images, detections, net)
+function feat = get_features(images, detections, net)
 %% extract the CNN features (using VGG model)
-f=[];
+feat = [];
 images = single(images);
 %obtain the rois & CNN features
 for i=1:size(detections,1)
-    rois=imcrop(images,detections(i,:));
+    rois = imcrop(images,detections(i,:));
     rois = imresize(rois, net.meta.normalization.imageSize(1:2),'method',net.meta.normalization.interpolation) ;
     rois = bsxfun(@minus, rois, net.meta.normalization.averageImage) ;
 
@@ -12,5 +12,5 @@ for i=1:size(detections,1)
     net.conserveMemory = 0; 
     net.eval({'input',rois}) ;
     f_rois = squeeze(squeeze(gather(net.vars(net.getVarIndex('x17')).value))) ; 
-    f = [f;f_rois'];
+    feat = [feat; f_rois'];
 end
